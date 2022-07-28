1ee3e45f3c08b646b0edf55ea0e0808906cdb432
aumcjpyang <66618954+aumcjpyang@users.noreply.github.com>
2022-07-28 16:30:09 +0800

    improve readability
    add a README.txt
    add some descriptions in .py to improve the readability of code

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is a tool to visualize tree distridutes with coordinates.
--- SakuraDistributeOnFoliumMap.py
+++ SakuraDistributeOnFoliumMap.py
@@ -2,11 +2,19 @@ import folium
 from folium.plugins import MarkerCluster
 import pandas as pd
 
+#create fmap
 fmap = folium.Map(location=[24.5271, 121.4899], zoom_start=12)
+
+#read in datas of trees
 tree_data = pd.read_csv('tree_atri_id.txt', encoding='big5')
+
+#specify the tree datas of sakura
 sdata = tree_data.loc['櫻花','經度':'數量']
+
+#add a cluster to contain all sakura trees
 cluster_sakuras = MarkerCluster().add_to(fmap)
 
+#add all sakura trees to the cluster
 for lat, lng, nums in zip(sdata['緯度'], sdata['經度'], sdata['數量']) :
     num = 0
     while num < nums :
